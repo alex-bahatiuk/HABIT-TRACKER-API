de8df47559ac929dd1661b4d0086ddf6e9b76739
Kacper Cholewiński <kacpercholewinski03@gmail.com>
2026-05-28 19:33:57 +0200

    new register and login tests

---
+++ tests/test_login.py
@@ -0,0 +1,33 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_login_valid_data_success():
+    email = "user@mail.com"
+    password = "password"
+    client.post("/auth/register", json={"email": email, "password": password})
+
+    login_res = client.post("/auth/login", json={"email": email, "password": password})
+
+    assert login_res.status_code == 200
+
+def test_login_unregistered_email_failure():
+    email = "user@mail.com"
+    password = "password"
+    client.post("/auth/register", json={"email": email, "password": password})
+
+    unregistered_email = "admin@mail.net"
+    login_res = client.post("/auth/login", json={"email": unregistered_email, "password": password})
+
+    assert login_res.status_code == 400
+
+def test_login_invalid_password_failure():
+    email = "user@mail.com"
+    password = "password"
+    client.post("/auth/register", json={"email": email, "password": password})
+
+    invalid_password = "drowssap"
+    login_res = client.post("/auth/login", json={"email": email, "password": invalid_password})
+
+    assert login_res.status_code == 400
---
+++ tests/test_register.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_register_valid_data_success():
+    email = "user@mail.com"
+    password = "password"
+    register_res = client.post("/auth/register", json={"email": email, "password": password})
+
+    assert register_res.status_code == 201
+
+    assert register_res.json()["email"] == email
+
+def test_register_invalid_email_format_failure():
+    email = "user.mail.com"
+    password = "password"
+    register_res = client.post("/auth/register", json={"email": email, "password": password})
+
+    print("print res: ", register_res)
+    print(register_res.json())
+    assert register_res.status_code == 422
+
+def test_register_empty_password_failure():
+    email = "user@mail.com"
+    password = ""
+    register_res = client.post("/auth/register", json={"email": email, "password": password})
+
+    assert register_res.status_code == 400
