41297806ac058a0e4ba50c82fcb694e1d28a0582
baraban888 <1975bahat@gmail.com>
2026-06-19 19:23:02 +0200

    Add password strength and common password validation

--- tests/test_register.py
+++ tests/test_register.py
@@ -27,3 +27,4 @@ def test_register_empty_password_failure():
     register_res = client.post("/auth/register", json={"email": email, "password": password})
 
     assert register_res.status_code == 422
+
